4fe80d8582be23674e2d07f030518e579637c15e
Ryan McFarland <ryanmcf10@gmail.com>
2020-12-01 15:34:22 -0500

    Pull scoreboard info from MLB website as XML. Create ScoreBoardCard component to display each game's data

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,53 @@
+
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <div class="columns is-multiline is-mobile">
+      <score-board-card
+        v-for="game in games"
+        v-bind:game="game"
+        v-bind:key="game.elements[0].id"
+      />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ScoreBoardCard from './components/ScoreBoardCard.vue'
+import convert from 'xml-js'
+import fetch from 'node-fetch'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ScoreBoardCard
+  },
+  data() {
+    return {
+      games: []
+    }
+  },
+  methods: {
+    fetchScoreBoard() {
+      var date = new Date('2020-09-01');
+
+      // get date components as zero-padded strings
+      var month = (date.getUTCMonth() + 1);
+      var day = date.getUTCDate();
+      var year = date.getUTCFullYear();
+
+      var url = `http://gd2.mlb.com/components/game/mlb/'year_${year}/month_${month}/day_${day}/scoreboard.xml`;
+      let data = {};
+
+      fetch(url).then(response => response.text()).then(str => {
+          data = JSON.parse(convert.xml2json(str));
+      }).then(() => {
+          this.games = data.elements[0].elements;
+          console.log(data.elements[0].elements);
+      });
+    }
+  },
+  mounted() {
+    this.fetchScoreBoard();
   }
 }
 </script>
---
+++ src/components/ScoreBoardCard.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class="column is-one-quarter">
+    <div class="scoreboard card" :id="game.elements[0].attributes.id">
+      <div class="card-header">
+        {{ game.elements[0].attributes.start_time }}
+        {{ game.elements[0].attributes.status }}
+      </div>
+      <div class="card-content">
+        <table class="table is-bordered">
+          <thead>
+            <tr>
+              <td></td>
+              <td>R</td>
+              <td>H</td>
+              <td>E</td>
+            </tr>
+          </thead>
+          <tbody>
+
+          <tr>
+            <td class="team away-team">
+              {{ game.elements[1].attributes.name }}
+            </td>
+            <td>
+              {{ game.elements[1].elements[0].attributes.R }}
+            </td>
+            <td>
+              {{ game.elements[1].elements[0].attributes.H }}
+            </td>
+            <td>
+              {{ game.elements[1].elements[0].attributes.E }}
+            </td>
+          </tr>
+          <tr>
+            <td class="team home-team">
+              {{ game.elements[2].attributes.name }}
+            </td>
+            <td>
+              {{ game.elements[2].elements[0].attributes.R }}
+            </td>
+            <td>
+              {{ game.elements[2].elements[0].attributes.H }}
+            </td>
+            <td>
+              {{ game.elements[2].elements[0].attributes.E }}
+            </td>
+
+          </tr>
+          </tbody>
+        </table>
+
+      </div>
+    </div>
+
+    </div>
+    </template>
+
+<script>
+export default {
+  name: 'ScoreBoardCard',
+  props: {
+    game: Object
+  }
+}
+</script>
+
+<style scoped>
+.team {
+  text-align: left;
+}
+</style>
